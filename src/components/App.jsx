de0ca50cd6c92af90e8cc2c4b9c200c7789c3bf1
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-04-25 20:30:05 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,115 @@
 
-import { Component} from "react"
-// import initialTodos from "../todo.json"
+import { useEffect, useState } from "react"
+import initialTodos from "../todo.json"
 import { TodoList } from "./TodoList/TodoList";
 import { TodoEditor } from "./TodoEditor/TodoEditor";
 import { Modal } from "./Modal/Modal";
 
 
-export class  App  extends Component{
-  state={
-    todos: [
-      { "id": "id-1", "text": "Вивчити основи React", "completed": true },
-      { "id": "id-2", "text": "Розібратися з React Router", "completed": false },
-      { "id": "id-3", "text": "Пережити Redux", "completed": false }
+export const  App = ()=>{
+  const [todos, setTodos] = useState(initialTodos)
+  const [showModal, setShowModal] = useState(false)
+  const [ filter, setFilter]= useState('')
+
+  const  addTodo=(labelText)=>{
+    const newTodo ={
+      id: Date.now(),
+      text: labelText,
+      completed: false
+    }
+
+
+useEffect(()=>{
+  const  todos = localStorage.getItem('todos')
+  if(todos){
+    const parsedTodos = JSON.parse(todos)
+    setTodos(parsedTodos)
+  }
+  console.log('Виклик useEffect');
+
+},[todos])
+
+    setTodos((prevState)=> [newTodo, ...prevState])
+  }
+
+    const  deleteTodo = (id) => {
+   setTodos((prevState) => prevState.filter((todo) => todo.id !== id),
+   )
+    };
+    const  toggleComplete=(id)=>{
+      setTodos((prevState)=>prevState.map((todo)=>(
+          todo.id == id ? {...todo, completed: !todo.completed} : todo
+        )))
+      
+      }
+
+const handelShowModal = ()=>{setShowModal((prevState)=>  !prevState.showModal
+)
+}
+
+  
+
+
+
+
+
+  
+
+  // state={
+  //   todos: [
+  //     { "id": "id-1", "text": "Вивчити основи React", "completed": true },
+  //     { "id": "id-2", "text": "Розібратися з React Router", "completed": false },
+  //     { "id": "id-3", "text": "Пережити Redux", "completed": false }
 
     
-    ],
-    showModal: false,
+    // ],
+    // showModal: false,
 
-    filter: '',
-
-
-
-  }
+    // filter: '',
 
 
 
+  
 
 
-  componentDidMount(){
-   const todos = localStorage.getItem('todos')
-   const parsedTodos =  JSON.parse(todos)
-   this.setState({todos: parsedTodos})
-   console.log(todos);
-  }
 
 
-  componentDidUpdate(prevProps, prevState){
-    if (prevState.todos ===  this.setState.todos ){
-return 
-    }
-    localStorage.setItem("todos", JSON.stringify(this.state.todos))
-    console.log(prevState.todos);
-    console.log(this.state.todos);
+
+//   componentDidMount(){
+//    const todos = localStorage.getItem('todos')
+//    const parsedTodos =  JSON.parse(todos)
+//    this.setState({todos: parsedTodos})
+//    console.log(todos);
+//   }
+
+
+//   componentDidUpdate(prevProps, prevState){
+//     if (prevState.todos ===  this.setState.todos ){
+// return 
+//     }
+//     localStorage.setItem("todos", JSON.stringify(this.state.todos))
+//     console.log(prevState.todos);
+//     console.log(this.state.todos);
    
    
-  }
+  // }
 
-  deleteTodo = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.filter((todo) => todo.id !== id),
-    }));
-  };
-
-addTodo=(labelText)=>{
-  const newTodo ={
-    id: Date.now(),
-    text: labelText,
-    completed: false
-  }
-
-  this.setState((prevState)=>({
-    todos: [...prevState.todos, newTodo]
-  }))
-}
-
-handelShowModal = ()=>{
-  this.setState((prevState)=>({
-    showModal: !prevState.showModal
-  }))
-}
-
-
-toggleComplete=(id)=>{
-this.setState((prevState)=>({
-  todos: prevState.todos.map((todo)=>(
-    todo.id == id ? {...todo, completed: !todo.completed} : todo
-  ))
-}))
-}
+ 
 
 
 
-render(){
+
+
   return(
 
     <>
- <TodoList  todos={this.state.todos} 
- toggleComplete={this.toggleComplete}
- onDelete = {this.deleteTodo}/>
-<button onClick={this.handelShowModal}>Відкрити модалку</button>
+ <TodoList  todos={todos} 
+ toggleComplete={toggleComplete}
+ onDelete = {deleteTodo}/>
+<button onClick={handelShowModal}>Відкрити модалку</button>
 
-{this.state.showModal && <Modal closeModal={this.handelShowModal}>
-<TodoEditor  addTodo={this.addTodo} onClose={this.handelShowModal}/>
+{showModal && <Modal closeModal={handelShowModal}>
+<TodoEditor  addTodo={addTodo} onClose={handelShowModal}/>
 </Modal>}
  {/* <Filter/>
 <Info/> */}
@@ -101,7 +119,7 @@ render(){
 }
 
 
-};
+
 
 
 
